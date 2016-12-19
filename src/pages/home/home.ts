4ed6da8c4e42ae06c8eb4bafb8849b560f26b2cc
Paul Sutherland <paul@liquidbinary.co.uk>
2016-12-19 13:17:11 +0000

    Add provider to demo issue

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { Database } from '../../providers/Database';
+
+
+
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [Database]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public database: Database) {
 
+  }
+
+  ionViewDidLoad(){
+    console.log('Home View Loaded')
   }
 
 }
